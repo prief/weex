f072890f22acc62a96cade6aa186b16418324de8
prief <207792089@qq.com>
2019-09-14 16:03:55 +0800

    add eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,12 @@ module.exports = {
     filename: "[name].js"
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      }
+    ]
   }
 };
